1e0132d8a94cce18dec211e474cc09484f0ff56f
Suess <justin.suess@westpoint.edu>
2022-09-07 12:35:26 -0400

    added files

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-function addText() {
-    var div = document.getElementById("main-contents");
-    var child = document.createTextNode("I'm text for the div");
-    div.appendChild(child); // Put the text node in the div
+
+var nodes  = []
+
+
+
+
+function loadPage(pageName) {
+    $("#main-contents").load(pageName);
 }
 
 
+window.addEventListener("hashchange", function() {
+    console.log(location.hash);
+    loadPage(location.hash.substring(1)+".html")
+  });
+  
