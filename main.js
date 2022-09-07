774853b86def41c0cecc759d022cf916d3618e5f
Suess <justin.suess@westpoint.edu>
2022-09-07 10:56:18 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function addText() {
+    var div = document.getElementById("main-contents");
+    var child = document.createTextNode("I'm text for the div");
+    div.appendChild(child); // Put the text node in the div
+}
+
+
